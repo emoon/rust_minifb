546cc762d8083fd10a0d29a07b199a53296bb028
Daniel Collin <daniel@collin.com>
2015-11-22 18:56:53 +0100

    Ran rustfmt

--- examples/noise.rs
+++ examples/noise.rs
@@ -4,9 +4,9 @@ const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
 
 fn main() {
-	let mut noise; 
-	let mut carry; 
-	let mut seed = 0xbeefu32;
+    let mut noise;
+    let mut carry;
+    let mut seed = 0xbeefu32;
 
     let mut buffer: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
 
@@ -15,17 +15,17 @@ fn main() {
     }
 
     while minifb::update(&buffer) {
-		for i in buffer.iter_mut() { 
-			noise = seed;
-			noise >>= 3;
-			noise ^= seed;
-			carry = noise & 1;
-			noise >>= 1;
-			seed >>= 1;
-			seed |= carry << 30;
-			noise &= 0xFF;
-			*i = (noise << 16) | (noise << 8) | noise; 
-		}
+        for i in buffer.iter_mut() {
+            noise = seed;
+            noise >>= 3;
+            noise ^= seed;
+            carry = noise & 1;
+            noise >>= 1;
+            seed >>= 1;
+            seed |= carry << 30;
+            noise &= 0xFF;
+            *i = (noise << 16) | (noise << 8) | noise;
+        }
     }
 
     minifb::close();
