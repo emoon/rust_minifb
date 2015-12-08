d4ca056adc9101e8c7eb86c0b691f085423d50c7
Daniel Collin <daniel@collin.com>
2015-12-08 22:57:14 +0100

    New version running with keyboard support

--- examples/noise.rs
+++ examples/noise.rs
@@ -6,27 +6,15 @@ const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
 fn main() {
-    //let mut noise;
-    //let mut carry;
-    //let mut seed = 0xbeefu32;
+    let mut noise;
+    let mut carry;
+    let mut seed = 0xbeefu32;
 
     let mut buffer: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
 
-    let window = match macos::Window::new("Noise Test - Press ESC to exit", 
-                                   WIDTH, 
-                                   HEIGHT, 
-                                   Scale::X1, 
-                                   Vsync::No) {
-        Ok(window) => window,
-        Err(info) => {
-            println!("{}", info);
-            return;
-        }
-    };
+    let mut window = Window::new("Noise Test - Press ESC to exit", WIDTH, HEIGHT, Scale::X1, Vsync::No).unwrap();
 
-    loop {
-        window.update(&buffer);
-        /*
+    while window.update(&buffer) {
         for i in buffer.iter_mut() {
             noise = seed;
             noise >>= 3;
@@ -38,6 +26,13 @@ fn main() {
             noise &= 0xFF;
             *i = (noise << 16) | (noise << 8) | noise;
         }
-        */
+
+        for key in window.get_keys().iter() {
+            match *key {
+                Key::A => println!("Pressed A"),
+                Key::B => println!("Pressed B"),
+                _ => (),
+            }
+        }
     }
 }
