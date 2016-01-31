cbf7c17c3a92d676f5707095781071016e8b90e1
Daniel Collin <daniel@collin.com>
2016-01-31 15:05:10 +0100

    Updated Windows version with WindowOptions

--- src/window_flags.rs
+++ src/window_flags.rs
@@ -1,5 +1,8 @@
+#[allow(dead_code)]
 const WINDOW_BORDERLESS: u32 = 1 << 1; 
+#[allow(dead_code)]
 const WINDOW_RESIZE: u32 = 1 << 2; 
+#[allow(dead_code)]
 const WINDOW_TITLE: u32 = 1 << 3; 
 
 use WindowOptions;
@@ -7,6 +10,7 @@ use WindowOptions;
 //
 // Construct a bitmask of flags (sent to backends) from WindowOpts
 //
+#[allow(dead_code)]
 pub fn get_flags(opts: WindowOptions) -> u32 {
     let mut flags = 0u32;
 
