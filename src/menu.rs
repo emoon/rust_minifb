db3dc0354e0967499c0653e694934df9115f6539
Daniel Collin <daniel@collin.com>
2016-02-24 22:31:31 +0100

    Added modifier key constats + some docs

--- src/menu.rs
+++ src/menu.rs
@@ -1,13 +1,33 @@
 use Key;
 
-pub const MENU_ID_SEPARATOR:usize = 0xffffffff;
+/// Command key on Mac OS
+pub const MENU_KEY_COMMAND: usize = 1;
+/// Windows key on Windows
+pub const MENU_KEY_WIN: usize = 2;
+/// Shift key
+pub const MENU_KEY_SHIFT: usize = 4;
+/// Control key
+pub const MENU_KEY_CTRL: usize = 8;
+/// Alt key
+pub const MENU_KEY_ALT: usize = 16;
 
+const MENU_ID_SEPARATOR:usize = 0xffffffff;
+
+///
+/// Used to hold the data for creating menus for the Application
+///
 pub struct Menu<'a> {
+    /// Name of the menu item
     pub name: &'a str,
+    /// User-defined Id thot will be sent back to the application in [get_menu_event]
     pub id: usize,
+    /// Shortcut key for the menu item
     pub key: Key,
+    /// Modifier on Windows for the menu
     pub modifier: usize,
+    /// Modifier on Mac OS
     pub mac_mod: usize,
+    /// Sub-menu. Vector of a sub-menu, otherwise None if no sub-menu
     pub sub_menu: Option<&'a Vec<Menu<'a>>>,
 }
 
