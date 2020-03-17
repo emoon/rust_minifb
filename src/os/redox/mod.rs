22d9f53df485b5471071384ef02696d0f4da2115
Melvillian <melvillevt@gmail.com>
2020-03-17 11:59:15 -0400

    add get_keys_released method to Window

--- src/os/redox/mod.rs
+++ src/os/redox/mod.rs
@@ -189,6 +189,10 @@ impl Window {
         self.key_handler.get_keys_pressed(repeat)
     }
 
+    pub fn get_keys_released(&self) -> Option<Vec<Key>> {
+        self.key_handler.get_keys_released()
+    }
+
     pub fn is_key_down(&self, key: Key) -> bool {
         self.key_handler.is_key_down(key)
     }
