a3b3b426605719663818b7ed05776d6a93e0ced3
Antonino Siena <a.siena@gmx.de>
2020-04-07 13:45:31 +0200

    Add transparency example

---
+++ examples/transparent.rs
@@ -0,0 +1,28 @@
+extern crate minifb;
+
+use minifb::{Key, ScaleMode, Window, WindowOptions};
+
+fn main() {
+    // Allocate the output buffer.
+    let buf = vec![0x00AAFF33; 320*480];
+
+    let mut window = Window::new(
+        "Press ESC to exit",
+        320 as usize,
+        480 as usize,
+        WindowOptions {
+            resize: true,
+            scale_mode: ScaleMode::Center,
+			borderless: true,
+			transparency: true,
+			..WindowOptions::default()
+        },
+    )
+    .expect("Unable to open Window");
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window
+            .update_with_buffer(&buf, 320, 480)
+            .unwrap();
+    }
+}
