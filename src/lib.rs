073a9d99714ecce539405dc86eee5ec5d11d3f40
Sean Cross <sean@xobs.io>
2021-03-13 17:53:30 +0800

    inputcallback: add a callback for key events
    Add a callback for key events. This includes all events such as
    Shift, not just text events.
    
    An empty default handler is provided for backwards compatibility.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/lib.rs
+++ src/lib.rs
@@ -83,10 +83,17 @@ pub enum CursorStyle {
     ResizeAll,
 }
 
-/// This trait can be implemented and set with ```set_input_callback``` to reieve a callback
-/// whene there is inputs incoming. Currently only support unicode chars.
+/// This trait can be implemented and set with ```set_input_callback``` to receive a callback
+/// when there is inputs.
 pub trait InputCallback {
+    /// Called when text is added to the window, or a key is pressed. This passes
+    /// in a unicode character, and therefore does not report control characters.
     fn add_char(&mut self, uni_char: u32);
+
+    /// Called whenever a key is pressed or released. This reports the state of the
+    /// key in the `state` argument, as well as the translated key in the `key` argument.
+    /// This includes control characters such as `Key::LeftShift`.
+    fn set_key_state(&mut self, _key: Key, _state: bool) {}
 }
 
 mod error;
