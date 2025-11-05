7363a5fa315385446d847681ddb2a86b471c17ad
Luca Casati <luk00casati@gmail.com>
2025-11-06 00:08:30 +0100

    fix wasm

--- src/os/wasm/mod.rs
+++ src/os/wasm/mod.rs
@@ -229,7 +229,9 @@ impl Window {
     pub fn set_rate(&mut self, _rate: Option<Duration>) {}
 
     #[inline]
-    pub fn get_rate(&self) -> Option<Duration> {}
+    pub fn get_rate(&self) -> Option<Duration> {
+        None
+    }
 
     #[inline]
     pub fn update_rate(&mut self) {}
